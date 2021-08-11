f347c60b1d72ee218dea5d7ac968117388a1d2bb
truong huy <truongnhhse140562@fpt.edu.vn>
2021-08-11 15:30:00 +0700

    update sign up

--- js/login.js
+++ js/login.js
@@ -4,7 +4,6 @@ function login() {
     var password = document.getElementById("password").value;
 
     if(userName === "truong" && password === "123"){
-        console.log(userName + " " + password);
         return true;
     }else{
         document.getElementById("errorMessage").innerHTML = "Wrong userName or password";
